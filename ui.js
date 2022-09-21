8d2a4b09a4b855eed07a1f1ab3ed853b4f4c1d8a
Gopal Joshi <gopal.josh73@gmail.com>
2022-09-21 19:31:11 +0400

    Displaying files

--- ui.js
+++ ui.js
@@ -11,6 +11,8 @@ async function displayUI() {
     signInButton.style = "display: none";
     var content = document.getElementById('content');
     content.style = "display: block";
+
+    displayFiles();
 }
 
 async function displayFiles() {
@@ -19,15 +21,17 @@ async function displayFiles() {
     while (ul.firstChild) {
         ul.removeChild(ul.firstChild);
     }
-    for (let file of files) {
-        if (!file.folder && !file.package) {
-            let a = document.createElement('a');
-            a.href = '#';
-            a.onclick = () => { downloadFile(file); };
-            a.appendChild(document.createTextNode(file.name));
-            let li = document.createElement('li');
-            li.appendChild(a);
-            ul.appendChild(li);
+    if (files) {
+        for (let file of files) {
+            if (!file.folder && !file.package) {
+                let a = document.createElement('a');
+                a.href = '#';
+                a.onclick = () => { downloadFile(file); };
+                a.appendChild(document.createTextNode(file.name));
+                let li = document.createElement('li');
+                li.appendChild(a);
+                ul.appendChild(li);
+            }
         }
     }
 }
